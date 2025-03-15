65bab64bf491853018ee0197d7218c560071c89a
Kaue-Francisco <kauesantos.francisco@gmail.com>
2025-03-15 18:54:53 -0300

    feat: Enhance Input component and Login page with controlled inputs

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -4,15 +4,17 @@ interface InputProps {
     label?: string;
     placeholder?: string;
     styleInput?: number;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({ label, placeholder, styleInput }: InputProps) {
+export default function Input({ label, placeholder, styleInput, value, onChange }: InputProps) {
     const inputClass = `style-input-${styleInput || 1}`;
     return (
         <div className="input-container">
             <div className={inputClass}>
                 {label && <label htmlFor="name">{label}</label>}
-                <input type="text" id="name" placeholder={placeholder}/>
+                <input type="text" id="name" placeholder={placeholder} value={value} onChange={onChange} />
             </div>
         </div>
     );
