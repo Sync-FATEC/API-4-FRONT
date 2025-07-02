3200471d08240c0462a9d8ba2cb179d76dc9f1cc
Filipe Colla <173160383+collafilipe@users.noreply.github.com>
2025-07-02 07:47:46 -0300

    Teste deployzudo (#92)
    Co-authored-by: Filipe Colla <filipe.francisco@autaza.com>

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -52,7 +52,7 @@ export default function LoginComponent() {
                     onChange={(e) => setPassword(e.target.value)} 
                 />
 
-                <p className="forget-password">Esqueceu sua senha? <a className="forget-password-click" onClick={handleForgetPassword}>Clique aqui</a></p>
+                <p className="forget-password">Esqueceu sua senha? <a className="forget-password-click" onClick={handleForgetPassword}>Clique aqui!</a></p>
 
             </div>
         </div>
